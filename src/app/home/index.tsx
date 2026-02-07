7cf01b2bd1f57aac41b2d0365e1050ac9648ac31
Aiken Tine Ahac <ahac.aiken@gmail.com>
2026-02-07 17:55:55 +0100

    feat: fix positioning

--- src/app/home/index.tsx
+++ src/app/home/index.tsx
@@ -1,19 +1,18 @@
-import { ScreenContainer } from "@/components/screen-container";
 import { VoiceChat } from "@/components/assistant/voice-chat";
 import { Aurora } from "@/shared/ui/molecules/aurora";
 import { View } from "react-native";
 
 export default function HomeScreen() {
   return (
-    <ScreenContainer>
-      <View className="flex-1 bg-background">
-        <View pointerEvents="none" className="absolute left-0 right-0 top-0 z-0">
-          <Aurora state="idle" height={220} />
-        </View>
-        <View className="z-10 flex-1 p-6">
-          <VoiceChat />
-        </View>
+    // <ScreenContainer>
+    <View className="flex-1 bg-background pt-12">
+      <View pointerEvents="none" className="absolute left-0 right-0 top-0 z-0">
+        <Aurora state="idle" height={220} />
       </View>
-    </ScreenContainer>
+      <View className="z-10 flex-1 p-6">
+        <VoiceChat />
+      </View>
+    </View>
+    // </ScreenContainer>
   );
 }
